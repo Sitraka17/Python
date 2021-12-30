e9591f60e456e616c78760b28e7e737c87785f42
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2021-12-30 23:28:48 +0100

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -24,3 +24,25 @@ conversion = np.array([0.0254,0.453592,1])
 
 # Print out product of np_baseball and conversion
 print(np_baseball*conversion)
+--------------------------------------------------------------------
+# np_baseball is available
+# Import numpy
+import numpy as np
+
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height. Replace 'None'
+med = np.median(np_baseball)
+print("Median: " + str(med))
+
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:,0],np_baseball[:,1])
+print("Correlation: " + str(corr))
+
+
