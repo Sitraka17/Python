af73546a6d3638e302b3e166f9be6bc5e68efdb6
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-05 22:37:02 +0100

    Update Matplotlib_as_plt.py
    https://matplotlib.org/

--- Matplotlib_as_plt.py
+++ Matplotlib_as_plt.py
@@ -11,3 +11,13 @@ import matplotlib.pyplot as plt
 plt.plot(year,pop)
 # Display the plot with plt.show()
 plt.show()
+
+#https://matplotlib.org/
+import numpy as np
+import matplotlib.pyplot as plt
+
+# this sets up the Matplotlib interactive windows:
+%matplotlib widget
+
+# this changes the default date converter for better interactive plotting of dates:
+plt.rcParams['date.converter'] = 'concise'
