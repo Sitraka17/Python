38eff0eb48a985400fd5d706965b2453cd17cfd1
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-07 12:13:24 +0100

    Update Matplotlib_as_plt.py

--- Matplotlib_as_plt.py
+++ Matplotlib_as_plt.py
@@ -68,3 +68,25 @@ plt.xticks(tick_val,tick_lab)
 
 # After customizing, display the plot
 plt.show()
+
+
+# Import numpy as np
+
+import numpy as np
+# Store pop as a numpy array: np_pop
+np_pop = np.array(pop)
+
+# Double np_pop
+np_pop*2
+# Update: set s argument to np_pop
+plt.scatter(gdp_cap, life_exp, s = pop)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
+
+# Display the plot
+plt.show()
