4df39a556cb5f1654f7d06d4ad604e91cb0e79c3
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-08 22:21:05 +0100

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,18 @@
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+# Get index of 'germany': ind_ger
+ind_ger= countries.index('germany')
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
+
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = { 'spain':'madrid', 'germany':'berlin','norway':'oslo' }
+
+# Print europe
+print(europe)
