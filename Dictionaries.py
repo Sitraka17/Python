130635236b667cb0033d797ccfc67102846025e5
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-08 22:22:32 +0100

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -12,7 +12,7 @@ countries = ['spain', 'france', 'germany', 'norway']
 capitals = ['madrid', 'paris', 'berlin', 'oslo']
 
 # From string in countries and capitals, create dictionary europe
-europe = { 'spain':'madrid', 'germany':'berlin','norway':'oslo' }
+europe = { 'spain':'madrid','france':'paris', 'germany':'berlin','norway':'oslo' }
 
 # Print europe
 print(europe)
