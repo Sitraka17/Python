dec792d8d489e3b92b6390de2754319fdeca0548
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-09 12:30:20 +0100

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -16,3 +16,12 @@ europe = { 'spain':'madrid','france':'paris', 'germany':'berlin','norway':'oslo'
 
 # Print europe
 print(europe)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
