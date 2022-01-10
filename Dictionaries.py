46c7d564779a78af1495fc9fad3a09942192af24
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-10 21:36:19 +0100

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -39,3 +39,23 @@ del(europe["australia"])
 
 # Print europe
 print(europe)
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france'])
+# Create sub-dictionary data
+data={'capital': 'rome', 'population': 59.83}
+# Add data to europe under key 'italy'
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } ,
+           'italy':{'capital': 'rome', 'population': 59.83}}
+# Print europe
+print(europe)
