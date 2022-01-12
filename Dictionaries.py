71012717a9c772f5382e30449b601a31908cef17
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-12 13:41:17 +0100

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -89,3 +89,21 @@ my_dict = {
 cars = pd.DataFrame(my_dict)
 # Print cars
 print(cars)
+
+#i should've used a .md; it would have looked nicer ^^
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(cars_dict)
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+cars.index =  ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+# Specify row labels of cars
+
+
+# Print cars again
+print(cars)
