88b8f27ca5981e2c55216f3b8d65015fc6239d02
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-01-12 13:23:10 +0100

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -59,3 +59,12 @@ europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
            'italy':{'capital': 'rome', 'population': 59.83}}
 # Print europe
 print(europe)
+
+
+#############################################
+dict = {
+"country":["Brazil","Russia","India","China","South Africa"],
+"capital":["Brasilia","Moscow","New Delhi","Beijing","Pretoria"],
+"area":[8.516, 17.10, 3.286, 9.597, 1.221]
+"population":[200.4, 143.5, 1252, 1357, 52.98] }
+####################################################### thus { then "key":[ "names"], again , finaly } 
