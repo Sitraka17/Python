1887681c4573e3b7b4c9bfd211b0fe1c099ad361
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-17 22:42:21 +0100

    Create PythonFromScratch.py

---
+++ PythonFromScratch.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns #the first words you will be able to read in any code is the import bloc. 
+			    #when you start anything you need to prepare a frame and the tools. 
+			    #packages are the tools, the ide is the frame. 
+
+#A variable is usefull
+#You will store informations. 
+
+ur_first_variable = "an unremarkable string" 
+#tada! It's stored!
+#but be carefull. It will not be shown yet. 
