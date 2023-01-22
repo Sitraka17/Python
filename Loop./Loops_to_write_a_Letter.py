eb62917aa1b52ed2a70b9a34a1b2b8804734c332
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-22 15:30:27 +0100

    Create Loops_to_write_a_Letter.py
    from turtle import *
    width(5)
    
    left(180)
    
    # Start of for loop.
    for i in range(5): # Code below is repeated 5 times
        # Code to be repeated 
        forward(50) # Indentation indcates code is inside the loop
        left(45) # Still inside the for loop
    # No more indentation! Marks the end of the for-loop.
    hideturtle()
    bye()

---
+++ Loop./Loops_to_write_a_Letter.py
@@ -0,0 +1,13 @@
+from turtle import *
+width(5)
+
+left(180)
+
+# Start of for loop.
+for i in range(5): # Code below is repeated 5 times
+    # Code to be repeated 
+    forward(50) # Indentation indcates code is inside the loop
+    left(45) # Still inside the for loop
+# No more indentation! Marks the end of the for-loop.
+hideturtle()
+bye()
