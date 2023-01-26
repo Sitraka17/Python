25f5762d839e237617c7a24d1e7468d47d7b5ce5
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-26 23:31:07 +0100

    Create datasetDOTcolumn.py

---
+++ Plot/datasetDOTcolumn.py
@@ -0,0 +1,22 @@
+# x should be ransom.letter and y should be ransom.frequency
+plt.plot(ransom.letter, ransom.frequency,
+         # Label should be "Ransom"
+         label="Ransom",
+         # Plot the ransom letter as a dotted gray line
+         linestyle=':', color='gray')
+
+# Display the plot
+plt.show()
+
+
+# Plot each line
+plt.plot(ransom.letter, ransom.frequency,
+         label='Ransom', linestyle=':', color='gray')
+
+# X-values should be suspect1.letter
+# Y-values should be suspect1.frequency
+# Label should be "Fred Frequentist"
+plt.plot(suspect1.letter, suspect1.frequency, label=suspect1)
+
+# Display the plot
+plt.show()
